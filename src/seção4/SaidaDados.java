6ad30f3498d95de35d00a4acf7280617179802af
Wesley Ferreira <wesleyfsilva97@gmail.com>
2026-02-04 23:15:12 -0300

    finaliza exercicio de fixacao de saida de dados

---
+++ src/seção4/SaidaDados.java
@@ -0,0 +1,29 @@
+package seção4;
+
+import java.util.Locale;
+
+public class SaidaDados {
+    public static void main(String[] args) {
+
+        int y = 28;
+        double x = 10.35498;
+        System.out.println(y);
+        System.out.printf("%.4f%n", x);
+        System.out.println("Olá mundo!");
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.4f%n", x);
+
+        double alt = 1.70;
+        String nome = "Wesley";
+        System.out.printf("Ola, meu nome é %s e eu tenho %.2f de altura%n ", nome, alt);
+
+        System.out.printf("Resultado = %.2f metros%n", alt);
+
+        String name = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", name, idade, renda);
+
+    }
+}
+
