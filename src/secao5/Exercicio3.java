5a038eedffc414f2c2a60561be1c05383e40e5f2
Wesley Ferreira <wesleyfsilva97@gmail.com>
2026-02-08 20:40:44 -0300

    implementa exercício 3 de estrutura condicional (são ou não multiplos)

---
+++ src/secao5/Exercicio3.java
@@ -0,0 +1,22 @@
+package secao5;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if(a % b == 0 || b % a == 0){
+            System.out.println("Os numeros são multiplos");
+        }
+        else {
+            System.out.println("Os numeros nao são multiplos");
+        }
+    }
+}
